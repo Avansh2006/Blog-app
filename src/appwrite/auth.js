a80980be353c11e2185da2937fab9f14ac26d623
Avansh Yadav <avanshyadav20072006@gmail.com>
2024-12-12 22:31:38 +0530

    Update auth.js

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -14,19 +14,29 @@ export class AuthService {
             
     }
 
-     async createAccount({ email, password, name }) {
-        try {
-            const userAccount = await this.account.create(ID.unique(), email, password, name);
-            if (userAccount) {
-                return this.login({ email, password });
-            } else {
-                return userAccount;
-            }
-        } catch (error) {
-            console.error("Error creating account:", error.message);
-            throw error;
+async createAccount({ email, password, name, userId = ID.unique() }) {
+    try {
+        if (!isValidUserId(userId)) {
+            throw new Error("Invalid userId: Must be at most 36 chars and contain valid characters.");
         }
+
+        const userAccount = await this.account.create(userId, email, password, name);
+        if (userAccount) {
+            return this.login({ email, password });
+        } else {
+            return userAccount;
+        }
+    } catch (error) {
+        console.error("Error creating account:", error.message);
+        throw error;
     }
+}
+
+function isValidUserId(userId) {
+    const regex = /^[a-zA-Z0-9][a-zA-Z0-9._-]{0,35}$/;
+    return regex.test(userId);
+}
+
 
     async login({ email, password }) {
         try {
